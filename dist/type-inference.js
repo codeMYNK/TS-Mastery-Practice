44051d4c64244d4279d519f6973961e700ca7023
Mayank Kumar <mynk8076@gmail.com>
2025-12-30 18:35:16 +0530

    Type Inference and Type Annotations

--- dist/type-inference.js
+++ dist/type-inference.js
@@ -11,4 +11,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //Ans: Type inference is a feature of typescript that allows the compiler to automatically infer the type of a variable based on the value assigned to it.
 let x = 10;
 console.log("X: ", x);
+//Que: What is type annotation in typescript?
+//Ans: Type annotation is a way to explicitly specify the type of a variable. It is a feature of typescript that allows the developer to specify the type of a variable.
+//Example Usage
+/*
+let x: number = 10;
+console.log("X: ", x);
+*/
+//How to declare a type annotation in typescript?
+//Ans: Type annotation is a feature of typescript that allows the developer to specify the type of a variable.
+let x1 = 10;
+console.log("X: ", x1);
 //# sourceMappingURL=type-inference.js.map
